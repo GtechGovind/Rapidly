c384eb92ee5a01d212a443cedc90c4eceaab8ffb
Govind Yadav <gtech.govind2000@gmail.com>
2024-04-08 21:58:34 +0530

    Rapidly v2024.1.1.2
    Change Log
    
    1. Implemented an administrative dashboard providing access to accounting details and reports. Additionally, incorporated user activation and deactivation functionality.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 2
-        versionName = "2024.1.1.2"
+        versionName = "2024.1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
